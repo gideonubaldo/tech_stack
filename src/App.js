c43d16d44ef7784afa698135d81023d810999ace
Gideon Ubaldo <gideon@tecarta.com>
2018-07-30 22:12:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+const App = () => {
+  return (
+    <View />
+
+  );
+};
+
+export default App;
